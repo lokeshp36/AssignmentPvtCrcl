a3249232c9c9a8e1dd3878934ddeef6a6f4036fe
lokesh p <lokeshpatidar63@gmail.com>
2020-12-24 19:24:30 +0000

    basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface Data {
+    date;
+    name;
+    number;
+    entities;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AssignmentPvtCrcl';
+
+  data: Data[] = [
+    {"date": new Date(2020, 8, 15), "name": 2012, "number": "Orange", "entities": "dsad231ff"},
+    {"date": new Date(2020, 4, 6), "name": 2011, "number": "Black", "entities": "gwregre345"},
+    {"date": new Date(2020, 2, 28), "name": 2005, "number": "Gray", "entities": "h354htr"},
+    {"date": new Date(2020, 7, 4), "name": 2003, "number": "Blue", "entities": "j6w54qgh"},
+    {"date": new Date(2020, 6, 31), "name": 1995, "number": "Orange", "entities": "hrtwy34"},
+    {"date": new Date(2020, 7, 9), "name": 2005, "number": "Black", "entities": "jejtyj"},
+    {"date": new Date(2020, 1, 19), "name": 2012, "number": "Yellow", "entities": "g43gr"},
+    {"date": new Date(2020, 9, 7), "name": 2013, "number": "Orange", "entities": "greg34"},
+    {"date": new Date(2020, 12, 13), "name": 2000, "number": "Black", "entities": "h54hw5"},
+    {"date": new Date(2020, 4, 25), "name": 2013, "number": "Red", "entities": "245t2s"}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
